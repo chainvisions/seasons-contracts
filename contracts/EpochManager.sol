a06be8f003df46e3f55edc90a47823e04767380a
chainvisions <chainvisions@outlook.com>
2021-01-16 14:19:44 -0700

    feat: Event emitting and parameter adjustment

--- contracts/EpochManager.sol
+++ contracts/EpochManager.sol
@@ -19,16 +19,21 @@ contract EpochManager {
     bool public managerInitialied;
     // SEED supply threshold for the epoch to advance.
     uint256 public supplyThreshold;
-    // Duration of BERRY burn period.
-    uin256 public berryBurnDuration;
+    // Duration of emission period.
+    uin256 public emissionPeriodDuration;
     // Epoch variable
     Epoch public epochs;
 
     struct Epoch {
         uint256 epochNo;
-        uint256 burnPeriodStartTime;
+        uint256 emissionPeriodEndTime;
         bool seedBurnEnabled;
     }
+
+    // Events for monitoring epoch advancements, BERRY burns and SEED burns.
+    event SeedsBurned(uint256 indexed burned, address indexed burner);
+    event BerryBurned(uint256 indexed burned, address indexed burner);
+    event EpochAdvanced(uint256 indexed previousEpoch, uint256 indexed newEpoch, address indexed caller);
 
     // @notice Initializes the Epoch Manager
     function initializeEpoch() public {
@@ -38,6 +43,8 @@ contract EpochManager {
     /// @notice Advance to the next epoch.
     /// @dev This triggers a new epoch cycle.
     function advanceEpoch() public {
+        require(SEEDS.totalSupply() <= supplyThreshold, "Epoch Manager: Supply threshold must be met to advance the epoch");
+
         // Increment the counter
         Epoch storage epoch = epochs;
         uint256 prevEpoch = epoch.epochNo;
@@ -45,12 +52,24 @@ contract EpochManager {
         epoch.epochNo = newEpoch;
 
         // Set the emission time
+        uint256 endTime = now.add(emissionPeriodDuration);
+        epoch.emissionPeriodEndTime = endTime;
         
+        emit EpochAdvanced(prevEpoch, newEpoch, msg.sender);
     }
 
     /// @notice Burns BERRY for SEEDS
-    function burnForSeeds() public {
+    function burnForSeeds(uint256 _amount) public {
+        emit BerryBurned(_amount, msg.sender);
+    }
 
+    /// @notice Burns SEEDS for BERRY
+    function burnForBerry(uint256 _amount) public {
+        emit SeedsBurned(_amount, msg.sender);
+    }
+
+    function adjustSupplyThreshold(_threshold) public {
+        supplyThreshold = _threshold;
     }
 
 }
